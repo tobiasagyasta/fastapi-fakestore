565dc31bc269d980ac9c230a177afa2c3d7521b7
Tobias Halomoan <agyasta1808@gmail.com>
2026-02-07 12:37:11 +0700

    fix : Allow all CORS access.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.infrastructure.db.engine import init_db
 from app.presentation.api.products_router import router as products_router
@@ -6,6 +7,13 @@ from app.presentation.api.products_router import router as products_router
 app = FastAPI()
 
 app.include_router(products_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
